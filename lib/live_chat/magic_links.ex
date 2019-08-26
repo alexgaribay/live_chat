3dc5b2ce5c42072be24bfdd614f1002495cd511f
Alex Garibay <alex@alexgaribay.com>
2019-08-26 14:15:34 -0600

    Update magic links docs

--- lib/live_chat/magic_links.ex
+++ lib/live_chat/magic_links.ex
@@ -1,5 +1,12 @@
 defmodule LiveChat.MagicLinks do
+  @moduledoc """
+  Helper for one-time token generation and verification.
+  """
+
+  # In production, use a stronger salt much longer than 4 characters :)
   @salt "magic"
+
+  @type user :: %{name: String.t(), email: String.t()}
 
   def child_spec(_) do
     %{
@@ -12,12 +19,20 @@ defmodule LiveChat.MagicLinks do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
-  def get_token(%{name: name, email: email} = user) do
+  @doc """
+  Creates a new token based on user information.
+  """
+  @spec generate_token(user()) :: String.t()
+  def generate_token(%{name: name, email: email} = user) do
     token = Phoenix.Token.sign(LiveChatWeb.Endpoint, @salt, "#{name}-#{email}")
     Agent.update(__MODULE__, fn map -> Map.put(map, token, user) end)
     token
   end
 
+  @doc """
+  Verifies a token. If valid, the user's information is returned.
+  """
+  @spec verify_token(String.t()) :: {:ok, user()} | {:error, :not_found}
   def verify_token(token) do
     case Agent.get_and_update(__MODULE__, fn map -> Map.pop(map, token) end) do
       nil -> {:error, :not_found}
