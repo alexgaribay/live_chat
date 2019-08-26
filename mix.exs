8eb2b432dc772c42a09025e2dc221e57613119f9
Alex Garibay <alex@alexgaribay.com>
2019-08-26 14:16:05 -0600

    Update Phoenix LiveView

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule LiveChat.MixProject do
       {:ecto, "~> 3.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", ref: "b44da5e"},
+      {:phoenix_live_view, "~> 0.1.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
