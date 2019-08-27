b4b765ab0d16d721815815f2487cff2989f7c11a
Chris Freeze <chrstnfreeze@gmail.com>
2019-08-27 15:53:03 -0600

    finish presence

--- lib/live_chat_web/live/sidebar_live.ex
+++ lib/live_chat_web/live/sidebar_live.ex
@@ -4,15 +4,18 @@ defmodule LiveChatWeb.SidebarLive do
   alias LiveChat.Presence
 
   def mount(%{visible: visible}, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(LiveChat.PubSub, "lobby:presence")
+    end
     assigns = [
       visible: visible,
-      users: %{"user_1" => nil}
+      users: %{}
     ]
 
     socket =
       socket
       |> assign(assigns)
-      |> handle_joins()
+      |> handle_joins(Presence.list("lobby:presence"))
 
     {:ok, socket}
   end
@@ -21,5 +24,25 @@ defmodule LiveChatWeb.SidebarLive do
     ChatView.render("sidebar.html", assigns)
   end
 
-  defp handle_joins(socket), do: socket
+  defp handle_joins(socket, joins) do
+    Enum.reduce(joins, socket, fn
+      {user, %{metas: [meta | _]}}, socket ->
+        assign(socket, :users, Map.put(socket.assigns.users, user, meta))
+    end)
+  end
+
+  defp handle_leaves(socket, leaves) do
+    Enum.reduce(leaves, socket, fn
+      {user, _}, socket ->
+        assign(socket, :users, Map.delete(socket.assigns.users, user))
+    end)
+  end
+
+  def handle_info(%Phoenix.Socket.Broadcast{event: "presence_diff", payload: diff}, socket) do
+    socket =
+      socket
+      |> handle_leaves(diff.leaves)
+      |> handle_joins(diff.joins)
+    {:noreply, socket}
+  end
 end
