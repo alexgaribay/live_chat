5363e73e8f92eef964ee59705b90fa0636631f71
Chris Freeze <chrstnfreeze@gmail.com>
2019-08-28 11:47:23 -0600

    finish local chat

--- lib/live_chat_web/live/chat_live.ex
+++ lib/live_chat_web/live/chat_live.ex
@@ -1,16 +1,45 @@
 defmodule LiveChatWeb.ChatLive do
   use Phoenix.LiveView
+  import Ecto.Changeset
+  alias LiveChatWeb.ChatView
 
   def mount(%{user: user}, socket) do
     assigns = [
-      user: user
+      user: user,
+      messages: [],
+      changeset: message_changeset()
     ]
     {:ok, assign(socket, assigns)}
   end
 
   def render(assigns) do
-    ~L"""
-    <div class="fullscreen">Welcome to chat, <%= @user.name %></div>
-    """
+    ChatView.render("chat.html", assigns)
+  end
+
+  def handle_event("send", %{"chat" => attrs}, socket) do
+    attrs
+    |> message_changeset()
+    |> case do
+      %{valid?: true, changes: %{message: message}} ->
+          chat_line = {socket.assigns.user, message}
+          assigns = [
+            messages: socket.assigns.messages ++ [chat_line],
+            changeset: message_changeset()
+          ]
+          {:noreply, assign(socket, assigns)}
+      %{valid?: false} = changeset ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  @types %{message: :string}
+  defp message_changeset(attrs \\ %{}) do
+    cast(
+      {%{}, @types},
+      attrs,
+      [:message]
+    )
+    |> validate_required([:message])
+    |> validate_length(:message, max: 100)
   end
 end
