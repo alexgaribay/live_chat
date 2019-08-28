6ca91f25947e2cd7bc94c2876455fcf6b2f64443
Chris Freeze <chrstnfreeze@gmail.com>
2019-08-28 16:08:57 -0600

    add emoji js to hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,14 +15,30 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-
+import EmojiConvertor from "emoji-js"
 import LiveSocket from "phoenix_live_view"
+
 let Hooks = {}
 Hooks.NewMessage = {
   mounted(){
     let messages = document.querySelector(".main")
     if((messages.scrollTop + messages.offsetHeight + this.el.offsetHeight) >= messages.scrollHeight){
       messages.scrollTop = messages.scrollHeight;
+    }
+    this.emojify()
+  },
+  updated(){
+    this.emojify()
+  },
+  emojify(){
+    let emoji = new EmojiConvertor
+    emoji.addAliases({
+      'thinking' : '1f914'
+    });
+    // Emojify on mount hook
+    let messageHTML = this.el.getElementsByTagName("p")[0]
+    if (messageHTML) {
+      messageHTML.innerHTML = emoji.replace_colons(messageHTML.innerHTML)
     }
   }
 }
